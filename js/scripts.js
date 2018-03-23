f9989b12fac94fa01d77e50a9be871a908dd4b49
Shane Frerichs <Guest@Epicodus-9C.local>
2018-03-23 14:06:40 -0700

    add clickable functions to see the results of the test, and updated the html code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var add = function(number1, number2, number3, number4, number5, number6, number7, number8, number9, number10, number11, number12, number13, number14, number15) {
+var add = function(number1, number2, number3, number4, number5) {
 
-  return number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10 + number11 + number12 + number13 + number14 + number15;
+  return number1 + number2 + number3 + number4 + number5;
 };
 
 $(document).ready(function() {
@@ -11,17 +11,13 @@ $(document).ready(function() {
     var number3 = parseInt($("#add3").val());
     var number4 = parseInt($("#add4").val());
     var number5 = parseInt($("#add5").val());
-    var number6 = parseInt($("#add6").val());
-    var number7 = parseInt($("#add7").val());
-    var number8 = parseInt($("#add8").val());
-    var number9 = parseInt($("#add9").val());
-    var number10 = parseInt($("#add10").val());
-    var number11 = parseInt($("#add11").val());
-    var number12 = parseInt($("#add12").val());
-    var number13 = parseInt($("#add13").val());
-    var number14 = parseInt($("#add14").val());
-    var number15 = parseInt($("#add15").val());
-    var result = add(number1, number2, number3, number4, number5, number6, number7, number8, number9, number10, number11, number12, number13, number14, number15);
-    $("#output").text(result);
+    var result = add(number1, number2, number3, number4, number5);
+    $(".output").text(result);
+    });
+    $("#score1").click(function() {
+      $(".java").show();
+
   });
-});
+
+
+  });
