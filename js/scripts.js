474c1e5041799e0dea2b8ee4848959f0e4633879
Shane Frerichs <Guest@Epicodus-9C.local>
2018-03-23 11:40:56 -0700

    ot will add new logic to the js files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-var add = function(number1, number2) {
-  return number1 + number2;
+var add = function(number1, number2, number3, number4, number5, number6, number7, number8, number9, number10, number11, number12, number13, number14, number15) {
+
+  return number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10 + number11 + number12 + number13 + number14 + number15;
 };
 
 $(document).ready(function() {
@@ -7,7 +8,20 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
+    var number2 = parseInt($("#add3").val());
+    var number2 = parseInt($("#add4").val());
+    var number2 = parseInt($("#add5").val());
+    var number2 = parseInt($("#add6").val());
+    var number2 = parseInt($("#add7").val());
+    var number2 = parseInt($("#add8").val());
+    var number2 = parseInt($("#add9").val());
+    var number2 = parseInt($("#add10").val());
+    var number2 = parseInt($("#add11").val());
+    var number2 = parseInt($("#add12").val());
+    var number2 = parseInt($("#add13").val());
+    var number2 = parseInt($("#add14").val());
+    var number2 = parseInt($("#add15").val());
+    var result = add(number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10 + number11 + number12 + number13 + number14 + number15);
     $("#output").text(result);
   });
 });
